06d043c59acc3656cfff3cae5e4bcd21841c9e18
Viet Quoc Tran <viet.tran@siemens.com>
2018-12-22 16:46:20 -0600

    _

---
+++ src/main/java/com/tutorbooking/core/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.tutorbooking.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+}
+
