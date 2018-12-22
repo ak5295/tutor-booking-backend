43e6a3c2e62f63611b7d290418d02b1fd8d3f508
Viet Quoc Tran <viet.tran@siemens.com>
2018-12-22 17:22:56 -0600

    _

--- src/main/java/com/tutorbooking/core/model/Tutor.java
+++ src/main/java/com/tutorbooking/core/model/Tutor.java
@@ -1,10 +1,13 @@
 package com.tutorbooking.core.model;
 
 
+import java.io.Serializable;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
-public class Tutor {
+@Data
+public class Tutor implements Serializable {
   private Long id;
   private String firstName;
   private String lastName;
