463c30a143c6466c0224b598f5602278a12b9400
Viet Quoc Tran <viet.tran@siemens.com>
2018-12-27 19:06:35 -0600

    student api

--- src/main/java/com/tutorbooking/core/controllers/StudentApi.java
+++ src/main/java/com/tutorbooking/core/controllers/StudentApi.java
@@ -1,10 +1,13 @@
 package com.tutorbooking.core.controllers;
 
+import com.tutorbooking.core.models.Student;
 import com.tutorbooking.core.services.repos.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +21,10 @@ public class StudentApi {
   public ResponseEntity<?> getStudents() {
     return new ResponseEntity<>(this.studentRepo.findAll(), HttpStatus.OK);
   }
+
+  @PostMapping("")
+  public ResponseEntity<?> createStudent(@RequestBody Student student) {
+    return new ResponseEntity<>(this.studentRepo.save(student), HttpStatus.ACCEPTED);
+  }
+
 }
